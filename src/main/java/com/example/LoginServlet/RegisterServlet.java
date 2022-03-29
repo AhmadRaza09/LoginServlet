f0f773d0a141e932ea1a7f6ee45c203fb33b1575
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-03-29 22:52:36 +0500

    added registerServlet. closes #5.

---
+++ src/main/java/com/example/LoginServlet/RegisterServlet.java
@@ -0,0 +1,23 @@
+package com.example.LoginServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("register.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        System.out.println("My email is: " + email);
+        System.out.println("My password is; " + password);
+    }
+}
